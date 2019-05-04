207b4fd2b1aa179c73a466f90fd6b7cde5a020f5
cyzuestc <421248827@qq.com>
2019-05-04 10:19:01 +0800

    增加建造者模式

---
+++ src/main/java/ink/cyz/learn/gof23/builder/AirShipDirector.java
@@ -0,0 +1,14 @@
+package ink.cyz.learn.gof23.builder;
+
+/**
+ * DATE: 2019/5/4 9:47
+ * AUTHOR: cyz.ink
+ **/
+public interface AirShipDirector {
+    /**
+     * 组装飞船对象
+     * @return
+     */
+    AirShip directAirship();
+
+}
